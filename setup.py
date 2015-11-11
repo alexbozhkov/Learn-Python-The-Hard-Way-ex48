499ed009de986bfb0ccb58a94a55f19bb1bf723d
Alexander Bozhkov <alexander.bojkov89@gmail.com>
2015-11-11 16:58:56 +0200

    README.txt

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'My Project',
-    'author': 'My Name',
+    'description': 'Learn Python The Hard Way ex48',
+    'author': 'Alexander Bozhkov',
     'url': 'URL to get it at.',
     'download_url': 'Where to download it.',
-    'author_email': 'My email.',
+    'author_email': 'alexander.bojkov89@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
     'packages': ['ex48'],
     'scripts': [],
-    'name': 'projectname'
+    'name': 'Learn Python The Hard Way ex48'
 }
 
 setup(**config)
