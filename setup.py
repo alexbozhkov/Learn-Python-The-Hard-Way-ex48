abb443d854200f64a34db83499956d2d143e05aa
abozhkov <alexander.bojkov89@gmail.com>
2022-11-22 14:09:55 +0200

    Hide info

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ config = {
     'author': 'Alexander Bozhkov',
     'url': 'URL to get it at.',
     'download_url': 'Where to download it.',
-    'author_email': 'alexander.bojkov89@gmail.com',
+    'author_email': 'test@mail.com',
     'version': '0.1',
     'install_requires': ['nose'],
     'packages': ['ex48'],
